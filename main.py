84954a1e0d8cbc1ee7e7a949ecde732f5d99394c
jecndlria <jecndlria@gmail.com>
2022-10-25 03:07:34 +0000

    implemented misplaced tile heuristic

--- main.py
+++ main.py
@@ -4,35 +4,34 @@ import puzzle, search
 from puzzle import goalState, veryEasy, easy, doable, oh_boy, impossible
 
 def main():
-    puzzlePrompt = input("This is an 8-Puzzle solver. Type \'0\' to use a default puzzle, or anything else to input your own puzzle.\n")
-    if puzzlePrompt == '0':
-        selectedDifficulty = input("Select the difficulty of the default puzzle from 0 to 5 (inclusive): ")
-        if selectedDifficulty == '0':
-            print("You have selected \'Trivial\'")
-            userPuzzle = goalState
-        if selectedDifficulty == '1':
-            print("You have selected \'Very Easy\'")
-            userPuzzle = veryEasy
-        if selectedDifficulty == '2':
-            print("You have selected \'Easy\'")
-            userPuzzle = easy
-        if selectedDifficulty == '3':
-            print("You have selected \'Doable\'")
-            userPuzzle = doable
-        if selectedDifficulty == '4':
-            print("You have selected \'Oh, boy\'")
-            userPuzzle = oh_boy
-        if selectedDifficulty == '5':
-            print("You have selected \'Impossible\'")
-            userPuzzle = impossible
-    else:
-        print("Enter a valid 8-puzzle configuration by each row, using a \'0\' to represent the blank spot. Separate each number with a space, and press ENTER when you finish a row.")
-        userRowOne = input("Enter Row #1: ")
-        userRowTwo = input("Enter Row #2: ")
-        userRowThree = input("Enter Row #3: ")
+    # puzzlePrompt = input("This is an 8-Puzzle solver. Type \'0\' to use a default puzzle, or anything else to input your own puzzle.\n")
+    # if puzzlePrompt == '0':
+    #     selectedDifficulty = input("Select the difficulty of the default puzzle from 0 to 5 (inclusive): ")
+    #     if selectedDifficulty == '0':
+    #         print("You have selected \'Trivial\'")
+    #         userPuzzle = goalState
+    #     if selectedDifficulty == '1':
+    #         print("You have selected \'Very Easy\'")
+    #         userPuzzle = veryEasy
+    #     if selectedDifficulty == '2':
+    #         print("You have selected \'Easy\'")
+    #         userPuzzle = easy
+    #     if selectedDifficulty == '3':
+    #         print("You have selected \'Doable\'")
+    #         userPuzzle = doable
+    #     if selectedDifficulty == '4':
+    #         print("You have selected \'Oh, boy\'")
+    #         userPuzzle = oh_boy
+    #     if selectedDifficulty == '5':
+    #         print("You have selected \'Impossible\'")
+    #         userPuzzle = impossible
+    # else:
+    #     print("Enter a valid 8-puzzle configuration by each row, using a \'0\' to represent the blank spot. Separate each number with a space, and press ENTER when you finish a row.")
+    #     userRowOne = input("Enter Row #1: ")
+    #     userRowTwo = input("Enter Row #2: ")
+    #     userRowThree = input("Enter Row #3: ")
 
-        userPuzzle = puzzle.createPuzzle(userRowOne, userRowTwo, userRowThree)
-
+    #     userPuzzle = puzzle.createPuzzle(userRowOne, userRowTwo, userRowThree)
     return
 
 if __name__ == "__main__":
--- search.py
+++ search.py
@@ -17,11 +17,28 @@ def generalSearch(puzzle: list[list[int]], heuristic):
 def checkLegalOperations(puzzle: list[list[int]]):
     return 0
 
-def uniformCostSearch(puzzle: list[list[int]]):
-    return 0
-
+# Intuition behind the Misplaced Tile Heuristic:
+    # It is trivial to search by row, since the goal of the puzzle is to sort each row in ascending order.
+    # This makes it so we can easily keep track of what number should be where using an incremented variable.
+    
+# Thus, the algorithm is as follows:
+    # 1. Initialize the variable "currentValue", which keeps track of the number that should be in the spot we look at.
+        # Initialize the variable "misplacedTiles", which keeps track of how many misplaced tiles we have seen thus far.
+    # 2. Iterate through the puzzle using a nested for loop. We will be searching each row from left to right... 
+        # ...which will make it so we will search the entire game board in (what should be) ascending order.
+    # 3. If the current value you see is not the blank (0), and it doesn't match the currentValue, increment misplacedTiles
+    # 4. Increment currentValue.
+    # 5. Terminate when you search the whole board and return misplacedTiles
+# This also easily scales up to larger puzzle sizes.
 def misplacedTileHeuristic(puzzle: list[list[int]]):
-    return 0
+    currentValue = 1
+    misplacedTiles = 0
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[i])):
+            if puzzle[i][j] != 0 and puzzle[i][j] != currentValue :
+                misplacedTiles += 1
+            currentValue += 1
+    return misplacedTiles
 
 def manhattanDistanceHeuristic(puzzle: list[list[int]]):
     return 0
