d5fd12bbfcd5eded9a1eb305a8d1cbe725da30f2
jecndlria <jecndlria@gmail.com>
2022-10-29 22:07:54 +0000

    moved notes, made code more organized, optimized manhattan distance

--- search.py
+++ search.py
@@ -1,14 +1,12 @@
-from distutils.command.build import build
 import anytree
+import heapq
 from puzzle import goalState
-
-visitedStates = {} # MAKE THIS A HASh TABLE!
-currentDepth = 0
-nodesExpanded = 0
-maxQueueSize = 0
+from collections import deque
 
 # This function is used to build the dictonary used in the Manhattan Distance Heuristic.
 # It builds upon initialization in order to save time and memory.
+# Also, it works independent of size. No need to change for different sizes of puzzle!
+# Note: This function can also be used to generate the goal state.
 def buildCorrectPairMappingDictionary(puzzle: list[list[int]]):
     correctPairMapping = {}
     currentValue = 1
@@ -16,12 +14,62 @@ def buildCorrectPairMappingDictionary(puzzle: list[list[int]]):
         for j in range(len(puzzle[i])):
             correctPairMapping[currentValue] = [i, j]
             currentValue += 1
+    correctPairMapping[currentValue] = 0                        # Last tile should always be 0
     return correctPairMapping
 
+visitedStates = {} # MAKE THIS A HASH TABLE, SO LOOKUP OF REPEATED STATES IS EASY!
+currentDepth = 0
+nodesExpanded = 0
+maxQueueSize = 0
 correctPairMapping = buildCorrectPairMappingDictionary(goalState)                          # Dictonary that maps the correct ordered pair for each block
 
-def generalSearch(puzzle: list[list[int]], heuristic):
+# function general-search(problem, QUEUEING-FUNCTION)
+#   nodes = MAKE-QUEUE(MAKE-NODE(problem.INITIAL-STATE))
+#   loop do
+#       if EMPTY(nodes) then return "failure"
+#       node = REMOVE-FRONT(nodes)
+#       if problem.GOAL-TEST(node.STATE) succeeds then return node
+#       nodes = QUEUEING-FUNCTION(nodes, EXPAND(node, problem.OPERATORS))
+#   end
+
+def generalSearch(puzzle: list[list[int]], queueingFunction: int):
     return 0
+
+
+def expandNode(puzzle: list[list[int]]):
+    return 0
+
+def misplacedTileHeuristic(puzzle: list[list[int]]):
+    currentValue = 1
+    misplacedTiles = 0
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[i])):
+            if puzzle[i][j] != 0 and puzzle[i][j] != currentValue:
+                misplacedTiles += 1
+            currentValue += 1
+    return misplacedTiles
+
+def manhattanDistanceHeuristic(puzzle: list[list[int]]):
+    # Initialize needed variables
+    currentValue = 1                                    # Keeps track of current block position
+    manhattanDistance = 0                               # Counter for manhattan distance
+    misplacedPairMapping = {}                           # Dictionary keeping track of the ordered pairs with incorrect blocks
+
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[i])):
+            if puzzle[i][j] != 0 and puzzle[i][j] != currentValue:      # If the current block isn't a blank AND it is misplaced...
+                misplacedPairMapping[puzzle[i][j]] = [i, j]             # Map the incorrect block value with the current pair.
+            currentValue += 1
+
+    for key in misplacedPairMapping:
+        misplacedPair = misplacedPairMapping[key]                       # Retrieve the pair with the incorrect block value.
+        misplacedPairValue = puzzle[misplacedPair[0]][misplacedPair[1]] # Retrieve the incorrect block value.
+        correctPair = correctPairMapping[misplacedPairValue]            # Use the incorrect block value to retrieve the correct ordered pair for that block value from the dictionary with the correct ordered pairs.
+        manhattanDistance += abs(misplacedPair[0] - correctPair[0]) + abs(misplacedPair[1] - correctPair[1])
+        
+    return manhattanDistance
+
+# --- BEGIN NOTES ---
 
 # Intuition behind checking the legal moves: Depends on location of '0'
     # If in the middle, then 4 legal moves.
@@ -29,8 +77,6 @@ def generalSearch(puzzle: list[list[int]], heuristic):
     # If in corner, then 2 legal moves.
 
 # A legal move is simply to swap the '0' with any adjacent element on the board.
-def expandNode(puzzle: list[list[int]]):
-    return 0
 
 # Intuition behind the Misplaced Tile Heuristic:
     # It is trivial to search by row, since the goal of the puzzle is to sort each row in ascending order.
@@ -45,15 +91,6 @@ def expandNode(puzzle: list[list[int]]):
     # 4. Increment currentValue.
     # 5. Terminate when you search the whole board and return misplacedTiles
 # This also easily scales up to larger puzzle sizes.
-def misplacedTileHeuristic(puzzle: list[list[int]]):
-    currentValue = 1
-    misplacedTiles = 0
-    for i in range(len(puzzle)):
-        for j in range(len(puzzle[i])):
-            if puzzle[i][j] != 0 and puzzle[i][j] != currentValue:
-                misplacedTiles += 1
-            currentValue += 1
-    return misplacedTiles
 
 # Intuition behind Manhattan Distance Heuristic:
     # As seen in the misplaced tile heuristic, it is straightforward to find the actual misplaced tiles.
@@ -77,23 +114,4 @@ def misplacedTileHeuristic(puzzle: list[list[int]]):
         # We can use another dictionary to save these pairs with the correct number, then we can simply do math with the ordered pairs to find the Manhattan distance!
         # Despite heavy memory usage (two dictionaries), this makes this function easy to scale up for bigger puzzle sizes.
 
-def manhattanDistanceHeuristic(puzzle: list[list[int]]):
-    # Initialize needed variables
-    currentValue = 1                                    # Keeps track of current block position
-    manhattanDistance = 0                               # Counter for manhattan distance
-    misplacedPairMapping = {}                           # Dictionary keeping track of the ordered pairs with incorrect blocks
-
-    for i in range(len(puzzle)):
-        for j in range(len(puzzle[i])):
-            correctPairMapping[currentValue] = [i, j]                   # Build dictionary of correct ordered pairs for blocks
-            if puzzle[i][j] != 0 and puzzle[i][j] != currentValue:      # If the current block isn't a blank AND it is misplaced...
-                misplacedPairMapping[puzzle[i][j]] = [i, j]             # Map the incorrect block value with the current pair.
-            currentValue += 1
-
-    for key in misplacedPairMapping:
-        misplacedPair = misplacedPairMapping[key]                       # Retrieve the pair with the incorrect block value.
-        misplacedPairValue = puzzle[misplacedPair[0]][misplacedPair[1]] # Retrieve the incorrect block value.
-        correctPair = correctPairMapping[misplacedPairValue]            # Use the incorrect block value to retrieve the correct ordered pair for that block value from the dictionary with the correct ordered pairs.
-        manhattanDistance += abs(misplacedPair[0] - correctPair[0]) + abs(misplacedPair[1] - correctPair[1])
-        
-    return manhattanDistance
+# --- END NOTES ---
